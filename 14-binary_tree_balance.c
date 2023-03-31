0fda85548b86593daad5cd6eb8f55a9b50a7d821
DanielMbithiMusau <daniel.musau@students.jkuat.ac.ke>
2023-03-31 13:21:46 +0300

    Balance of tree

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -26,22 +26,6 @@ size_t recursive_height(const binary_tree_t *tree)
 }
 
 /**
- * binary_tree_height - Measures the height of a binary tree.
- *
- * @tree: Is a pointer to the root node of the tree to
- * measure the height.
- *
- * Return: The height of tree else 0.
- */
-size_t binary_tree_height(const binary_tree_t *tree)
-{
-	if (tree == NULL)
-		return (0);
-
-	return (recursive_height(tree) - 1);
-}
-
-/**
  * binary_tree_balance - measures the balance factor of a binary tree.
  *
  * @tree: is a pointer to the root node of the tree to measure
@@ -51,13 +35,14 @@ size_t binary_tree_height(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	int left_height, right_height;
+	int left_height = 0;
+	int right_height = 0;
 
 	if (tree == NULL)
 		return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+	left_height = recursive_height(tree->left);
+	right_height = recursive_height(tree->right);
 
 	return (left_height - right_height);
 }
